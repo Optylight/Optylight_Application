74a3f718cfebeb1b63c042add7e66c41533ca9b3
Optylight <optylight@yahoo.com>
2017-04-10 17:09:41 +0200

    Update

--- app/src/main/java/optylight/norlia/net/MainActivity.java
+++ app/src/main/java/optylight/norlia/net/MainActivity.java
@@ -1,16 +1,12 @@
 package optylight.norlia.net;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-
-
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     // Used to load the 'native-lib' library on application startup.
@@ -18,20 +14,67 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    ImageButton myImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        public void tarteauxpommes(View view)
-        {
-            Intent intent = new Intent(MainActivity.this, Chambre.class);
-            startActivity(intent);
-        }
+        myImageButton = (ImageButton) findViewById(R.id.my_image_button);
 
+        myImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, Chambre.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        ImageButton myImageButton2;
+
+        myImageButton2 = (ImageButton) findViewById(R.id.my_image_button2);
+
+        myImageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, salon.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        ImageButton myImageButton3;
+
+        myImageButton3 = (ImageButton) findViewById(R.id.my_image_button3);
+
+        myImageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, cuisine.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+        ImageButton myImageButton4;
+
+        myImageButton4 = (ImageButton) findViewById(R.id.imageButton8);
+
+        myImageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, entree.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+        ImageButton myImageButton5;
+
+        myImageButton5 = (ImageButton) findViewById(R.id.imageButton2);
+
+        myImageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLoadNewActivity = new Intent(MainActivity.this, entree.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
-
-
-
 }
--- app/src/main/java/optylight/norlia/net/salon.java
+++ app/src/main/java/optylight/norlia/net/salon.java
@@ -2,18 +2,12 @@ package optylight.norlia.net;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
-public class Chambre extends AppCompatActivity {
+public class salon extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chambre);
-
-
+        setContentView(R.layout.activity_salon);
     }
 }
