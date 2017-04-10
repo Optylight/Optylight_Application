a3f5015ec2a55f6d2c3feddd066bbc870b7c5bd6
optylight <optylight@yahoo.com>
2017-04-10 15:16:06 +0200

    Update MainActivity

---
+++ app/src/main/java/optylight/norlia/net/Chambre.java
@@ -0,0 +1,14 @@
+package optylight.norlia.net;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chambre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chambre);
+
+    }
+}
--- app/src/main/java/optylight/norlia/net/MainActivity.java
+++ app/src/main/java/optylight/norlia/net/MainActivity.java
@@ -2,19 +2,33 @@ package optylight.norlia.net;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        public void tarteauxpommes(View view)
+        {
+            Intent intent = new Intent(MainActivity.this, Chambre.class);
+            startActivity(intent);
+        }
 
     }
 
